54125c9944e0c8ae51859618a7819b029cd63c50
Swapna <Swapna@example.com>
2026-01-07 19:26:54 +0530

    these are terraform changes

---
+++ terraform_Practice/main.tf
@@ -0,0 +1,32 @@
+
+
+
+
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+
+  required_version= ">=1.2.0"
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+  
+}
+
+
+resource "aws_instance" "app_server" {
+   ami = "ami-0c398cb65a93047f2"
+
+   instance_type="t3.micro"
+   
+   tags={
+     Name="Terraform_practice"
+   }
+}
